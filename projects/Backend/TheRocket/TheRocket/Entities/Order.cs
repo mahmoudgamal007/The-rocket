731ed5826256c2eade5e505e3cff745e55d22187
MennaMKhalil <mennamokhalil@gmail.com>
2022-11-07 17:54:13 +0200

    Adding Order and ReserveCart Entities

---
+++ projects/Backend/TheRocket/TheRocket/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace TheRocket.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string DeliveryStatus { get; set; }
+        public bool IsReturned { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public int Quantity { get; set; }
+        public bool ReturnRequest { get; set; }
+
+
+
+    }
+}
---
+++ projects/Backend/TheRocket/TheRocket/Entities/ReserveCart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TheRocket.Entities
+{
+    public class ReserveCart
+    {
+
+        public int Quantity { get; set; }
+        public bool IsSubmitted { get; set; }
+
+       
+
+    }
+}
