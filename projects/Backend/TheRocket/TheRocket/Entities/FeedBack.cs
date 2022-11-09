20c3ad39cbc8da292d81d461f496945fa8c02269
Asmaa Ashraf <114494581+AsmaaAshraf707@users.noreply.github.com>
2022-11-09 19:16:23 +0200

    add 2 new classes ( Feedback & Subscrip )

---
+++ projects/Backend/TheRocket/TheRocket/Entities/FeedBack.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TheRocket.Entities
+{
+    public class FeedBack
+    {
+        [Key]
+        public int FeedBack_Id { get; set; }
+        public string Comment { get; set; }
+        public int Rating { get; set; }
+
+    }
+}
---
+++ projects/Backend/TheRocket/TheRocket/Entities/Subscrip.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheRocket.Entities
+{
+    public class Subscrip
+    {
+        [Key]
+        public int SubscripId { get; set; }
+        public bool IsSubscribed { get; set; }
+        public int DisCount { get; set; }
+        public decimal Price { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
