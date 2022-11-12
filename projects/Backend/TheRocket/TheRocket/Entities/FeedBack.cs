c1688862c8b5316a650ae274ff8cb16c86afa80b
Asmaa Ashraf <114494581+AsmaaAshraf707@users.noreply.github.com>
2022-11-12 15:52:43 +0200

    Edit 2 Classes (Feedback & Subscrip)

--- projects/Backend/TheRocket/TheRocket/Entities/FeedBack.cs
+++ projects/Backend/TheRocket/TheRocket/Entities/FeedBack.cs
@@ -4,7 +4,7 @@ namespace TheRocket.Entities
     public class Feedback:BaseEntity
     {
         [Key]
-        public int Feedback_Id { get; set; }//just Id
+        public int Id { get; set; }
         public string? Comment { get; set; }
         [Range(1,5)]
         public int Rating { get; set; }
--- projects/Backend/TheRocket/TheRocket/Entities/Subscrip.cs
+++ projects/Backend/TheRocket/TheRocket/Entities/Subscrip.cs
@@ -5,10 +5,10 @@ namespace TheRocket.Entities
     public class Subscrip:BaseEntity
     {
         [Key]
-        public int SubscripId { get; set; }//Just Id
-        public bool IsSubscribed { get; set; }//IsActivated
-        public int DisCount { get; set; }//Discount
-        public decimal Price { get; set; }//Double
+        public int Id { get; set; }
+        public bool IsActivated { get; set; }
+        public int Discount { get; set; }
+        public double Price { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
 
