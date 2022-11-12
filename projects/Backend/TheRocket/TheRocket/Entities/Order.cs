c72329ff67e2f30a95449982f0c2071ec433556d
Abdulrahman Hassan Shabaka <73299367+Abdulrahman-Shabaka@users.noreply.github.com>
2022-11-12 14:07:46 +0200

    add some comments to the entites plz check them

--- projects/Backend/TheRocket/TheRocket/Entities/Order.cs
+++ projects/Backend/TheRocket/TheRocket/Entities/Order.cs
@@ -1,17 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace TheRocket.Entities
 {
-    public class Order
+    public class Order:BaseEntity
     {
+        [Key]
         public int Id { get; set; }
-        public string DeliveryStatus { get; set; }
+        public string DeliveryStatus { get; set; }//need to be enum
         public bool IsReturned { get; set; }
         public DateTime ReturnDate { get; set; }
         public DateTime DeliveryDate { get; set; }
         public int Quantity { get; set; }
         public bool ReturnRequest { get; set; }
-
-
 
     }
 }
--- projects/Backend/TheRocket/TheRocket/Entities/Plan.cs
+++ projects/Backend/TheRocket/TheRocket/Entities/Plan.cs
@@ -1,16 +1,16 @@
 ﻿namespace TheRocket.Entities
 {
-    public class Plan
+    public class Plan:BaseEntity
     {
-        public int PlanId { get; set; }
-        public string PlanName { get; set; }
+        public int PlanId { get; set; }//just Id
+        public string PlanName { get; set; }//just Name
         public string Description { get; set; }
-        public double CurrentPrice { get; set; }
+        public double CurrentPrice { get; set; }//Price
         public int Duration { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public DateTime CreatedDate { get; set; }//remove
         public int Discount { get; set; }
-        public string CreatedBy { get; set; }
-        public bool IsDeleted { get; set; } 
+        public string CreatedBy { get; set; }//remove
+        public bool IsDeleted { get; set; } //remove
 
     }
 }
