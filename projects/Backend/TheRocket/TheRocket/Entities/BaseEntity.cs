62c6eb1002f932e5f5ffbc5aab257a41a0f6b557
Abdulrahman Hassan Shabaka <73299367+Abdulrahman-Shabaka@users.noreply.github.com>
2022-11-16 20:14:57 +0200

    add default value to is deleted =fals

--- projects/Backend/TheRocket/TheRocket/Entities/BaseEntity.cs
+++ projects/Backend/TheRocket/TheRocket/Entities/BaseEntity.cs
@@ -3,7 +3,7 @@ namespace TheRocket.Entities
 {
     public class BaseEntity
     {
-        public bool IsDeleted { get; set; }
+        public bool IsDeleted { get; set; } = false;
         public DateTime CreatedAt { get; set; }
         public int CreatedBy {get;set;}
     }
