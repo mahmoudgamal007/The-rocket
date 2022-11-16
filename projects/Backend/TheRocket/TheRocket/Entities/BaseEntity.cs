002573b8e595cb914ba82c97c1acb159b61e2225
Abdulrahman Hassan Shabaka <73299367+Abdulrahman-Shabaka@users.noreply.github.com>
2022-11-16 20:16:43 +0200

    add default value to created at =dateTime.now

--- projects/Backend/TheRocket/TheRocket/Entities/BaseEntity.cs
+++ projects/Backend/TheRocket/TheRocket/Entities/BaseEntity.cs
@@ -4,7 +4,7 @@ namespace TheRocket.Entities
     public class BaseEntity
     {
         public bool IsDeleted { get; set; } = false;
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
         public int CreatedBy {get;set;}
     }
 }
