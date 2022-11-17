331488e644bc869d3c220af373275910bd96c616
MennaMKhalil <mennamokhalil@gmail.com>
2022-11-17 20:14:53 +0200

    Fixing error in program.cs

--- projects/Backend/TheRocket/TheRocket/Program.cs
+++ projects/Backend/TheRocket/TheRocket/Program.cs
@@ -1,93 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using TheRocket.Repositories.RepoInterfaces;
+using TheRocket.Entities.Users;
+using TheRocket.TheRocketDbContexts;
 using Microsoft.EntityFrameworkCore;
-<<<<<<< HEAD
-using TheRocket.Entities.Users;
 using TheRocket.Repositories;
-using TheRocket.TheRocketDbContexts;
-using TheRocket.Repositories.RepoInterfaces;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
-
-
-builder.Services.AddDbContext<TheRocketDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
-
-builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<TheRocketDbContext>();
-
-builder.Services.AddScoped<ISubCategory, SubCategoryRepo>();
-
-
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
-||||||| 1cc5b44
-using TheRocket.Entities.Users;
-using TheRocket.TheRocketDbContexts;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<TheRocketDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
-
-builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<TheRocketDbContext>();
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
-=======
-using TheRocket.Entities.Users;
-using TheRocket.TheRocketDbContexts;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -100,12 +16,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
-
 builder.Services.AddDbContext<TheRocketDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<TheRocketDbContext>();
- builder.Services.AddScoped<IPlanRepository>();
+builder.Services.AddScoped<IPlanRepository>();
+builder.Services.AddScoped<ISubCategory,SubCategoryRepo>();
 
 
 var app = builder.Build();
@@ -118,12 +34,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
-
->>>>>>> 461ae08990cba62971b55adf7ce685ad0e41a4f7
