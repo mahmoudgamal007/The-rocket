f91fff31a4d60bacd5dcf7a52a231a25d9b6efe9
Abdelsalam Seleim <abdelsalamseleim@gmail.com>
2022-11-23 17:01:39 +0200

    Fixed Head Conflict

--- projects/Frontend/client/src/app/app-routing.module.ts
+++ projects/Frontend/client/src/app/app-routing.module.ts
@@ -1,33 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-const routes: Routes = [
-  
-=======
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
 import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   { path: 'product', loadChildren: () => import('./product/product.module').then(mod => mod.ProductModule) }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
 ];
 
 @NgModule({
