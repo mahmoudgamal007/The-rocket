0ef032b0a131c9fde4ef08a5449233846ad24e06
Abdelsalam Seleim <abdelsalamseleim@gmail.com>
2022-11-23 17:01:06 +0200

    Fixed Head Conflict

--- projects/Frontend/client/src/app/app.module.ts
+++ projects/Frontend/client/src/app/app.module.ts
@@ -1,91 +1,25 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxSpinnerModule } from 'ngx-spinner';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-@NgModule({
-  declarations: [
-    AppComponent
-=======
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
     NgxSpinnerModule,
     HttpClientModule
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
-    NgxSpinnerModule,
-    HttpClientModule
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
-    NgxSpinnerModule,
-    HttpClientModule
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
-=======
-    NgxSpinnerModule,
-    HttpClientModule
->>>>>>> 3a18350ded735fc0d173dce8cf72c8ff8c23eba9
   ],
   providers: [],
   bootstrap: [AppComponent]
