529b13836a2aae7f28bbae66acaff236ae5ed1c9
mahmoud gamal <mahmoudgamal.gm141@gmail.com>
2022-11-28 16:46:34 +0200

    addind seller

--- projects/Frontend/client/src/app/seller/seller.component.ts
+++ projects/Frontend/client/src/app/seller/seller.component.ts
@@ -1,7 +1,11 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { AccountService } from '../account/account.service';
 import { ProductService } from '../product/product.service';
+import { IAppUser } from '../shared/models/appUser';
 import { IProduct } from '../shared/models/product';
-import { shopParams } from '../shared/models/shopParams';
+import { IUser } from '../shared/models/user';
 import { SellerService } from './seller.service';
 
 @Component({
@@ -10,69 +14,44 @@ import { SellerService } from './seller.service';
   styleUrls: ['./seller.component.scss']
 })
 export class SellerComponent implements OnInit {
-  @ViewChild('search', { static: false }) searchTermVC!: ElementRef;
-  products: IProduct[] | undefined = [];
-  shopParams = new shopParams();
-  totalCount: number = 0;
-  sortOptions = [
-    { name: 'Alphabetical', value: 'Name' },
-    { name: 'Price', value: 'Price' },
-    { name: 'Brand', value: 'Brand' },
-    { name: 'Seller', value: 'SellerId' },
-    { name: 'Category', value: 'subCategoryId' },
-  ];
-  orderOptions = [
-    { name: 'Ascending', value: 'asc' },
-    { name: 'Descending', value: 'desc' },
-  ];
-
-
-  constructor(private sellerService: SellerService) { }
+  appUser!: IAppUser;
+  currentUser$!:Observable<IUser|null>;
+  id!: any;
+  products:IProduct[]=[];
+  constructor(private accountService : AccountService ,private sellerService : SellerService ) { }
 
   ngOnInit(): void {
-    this.getProducts();
+    this.currentUser$=this.accountService.currentUser$;
+
+    //  this.currentUser$.subscribe(response=>{this.id=response!.userId;},error=>{console.log(error);})
+    // this.id=this.currentUser$.subscribe(response=>{this.id=response?.userId;},error=>{console.log(error);})
+    //this.currentUser$.pipe(tap(res=>this.id=res?.userId))
+    // this.getproducts();
+     console.log(this.id);
   }
-
-  getProducts() {
-    this.sellerService.getProducts(this.shopParams).subscribe(
-      (response) => {
-        this.products = response!.products;
-        this.totalCount = response!.productMatchCount;
-      },
-      (error) => { console.log(error) });
+  getproducts()
+  {
+    
+    return this.sellerService.getProducts().subscribe(resp=>{this.products=resp;},error=>{console.log(error);})
   }
-
-  onSearch() {
-    this.shopParams.searchTerm = this.searchTermVC.nativeElement.value;
-    this.shopParams.pageNumber = 1;
-    this.getProducts();
-  }
-
-  onReset() {
-    this.searchTermVC.nativeElement.value = '';
-    this.shopParams = new shopParams();
-    this.getProducts();
-  }
-
-  onSortSelected(sort: string) {
-    this.shopParams.sortBy = sort;
-    this.getProducts();
-  }
-
-  onOrderSelected(order: string) {
-
-    this.shopParams.sortOrder = order;
-    this.getProducts();
-  }
-
-  onPageChanged(event: any) {
-    if (this.shopParams.pageNumber !== event) {
-      this.shopParams.pageNumber = event;
-      this.getProducts();
-    }
-  }
-
-
- 
+      
+    // this.getUser();
+  
+    
+    
+  // }
+  // getUser()
+  // {
+  //   this.currentUser$.subscribe
+  //   (
+  //     response=>{this.id=response?.userId;},
+  //     error=>{console.log(error);}
+  //   )
+  //   return this.sellerService.getUser(this.id).subscribe(
+  //     response=>{this.appUser=response;},
+  //     error=>{console.log(error);}
+      
+  //   )
+  // }
 
 }
--- projects/Frontend/client/src/app/seller/seller.service.ts
+++ projects/Frontend/client/src/app/seller/seller.service.ts
@@ -1,11 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { IAppUser } from '../shared/models/appUser';
+import { IProduct } from '../shared/models/product';
+import { IUser } from '../shared/models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SellerService {
-  baseUrl: string = environment.apiUrl + '/';
+  currentUser$!:Observable<IUser|null>;
+  baseUrl: string = environment.apiUrl ;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  getUser(userId:any)
+  {
+    return this.http.get<IAppUser>(this.baseUrl+'/AppUser/GetAppUserByUserId?AppUserId='+userId.toString())
+  }
+  getProducts()
+  {
+    return this.http.get<IProduct[]>(this.baseUrl+'Product/action?sellerId=6')
+  }
+ 
+
+  
 }
