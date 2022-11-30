6657e64f78e399325dba809952231e9f5b3b3938
Abdulrahman Hassan Shabaka <73299367+Abdulrahman-Shabaka@users.noreply.github.com>
2022-11-30 12:08:04 +0200

    add service to upload image

--- projects/Frontend/client/src/app/seller/seller-routing.module.ts
+++ projects/Frontend/client/src/app/seller/seller-routing.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { SellerComponent } from './seller.component';
 import { SellerOrdersComponent } from './seller-orders/seller-orders.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 
 const routes: Routes = [
   { path: '', component: SellerComponent},
   { path: 'orders', component: SellerOrdersComponent },
+  { path: 'addProduct', component: AddProductComponent }
+
 
 ]
 @NgModule({
